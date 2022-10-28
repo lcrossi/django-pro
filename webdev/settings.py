07808f74f0b607508412e823b4910ba772e3cc73
Luis Rossi <lcrossi2010@gmail.com>
2022-10-28 09:09:22 -0300

    [fix]: security

--- webdev/settings.py
+++ webdev/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from ..secrets import secretKeyWebdev
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n4wxvt^-k$olwnk5ypgtt2rzew@=*o(ces3q+fjl0z)qf*%-lc'
+SECRET_KEY = secretKeyWebdev
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
